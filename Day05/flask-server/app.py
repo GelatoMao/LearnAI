9967a896d1a4f22eff9f0bdfa793040e255f8a46
GelatoMao <gelatomao@outlook.com>
2021-01-30 22:19:56 +0800

    add flask CORS

--- Day05/flask-server/app.py
+++ Day05/flask-server/app.py
@@ -3,10 +3,12 @@ from flask import Flask
 from db_config import app
 # 导入user模块
 from handler.user import user
+from flask_cors import CORS
 
 # 注册蓝图
 app.register_blueprint(user, url_prefix="/user")
-
+#安装flask_cors 设置允许跨域
+CORS(app, supports_credentials=True)
 
 # 商品模块
 # 订单模块
